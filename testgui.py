c63f0f6f5e4e0614069f0a39adf5249d3d5ffb1f
Alex Wang <alex.wang@Alex-Wang.local>
2018-09-27 09:40:01 -0400

    adding files

---
+++ testgui.py
@@ -0,0 +1,27 @@
+'''
+test.py - basic ugly gui
+alexwang - original coding, Sept. 21, 2018
+'''
+
+
+
+from tkinter import *
+master = Tk()
+master.config(background="black")
+label = Label(master, text="Hockey Pool")
+label.config(background="black")
+label.config(foreground="white")
+label.pack()
+button = Button(master, text="QUIT", fg="red", command=quit)
+
+button.pack(side=BOTTOM)
+listbox = Listbox(master)
+listbox.config(background="black")
+listbox.config(foreground="white")
+listbox.pack()
+listbox.insert(END, "Player, Goals")
+lst = [["connor mcdavid", 208], ["sidney crosby", 234], ["steven stamkos", 187], ["austin matthews", 70]]
+for item in lst:
+	listbox.insert(END, item[0] + "-" + str(item[1]))
+	
+mainloop()
